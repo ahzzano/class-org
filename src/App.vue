9c8f87d48b0f774d9ce7265f44ee69757bd44ef0
W. Santos <w.santos.gg@gmail.com>
2022-08-26 21:32:46 +0800

    simplified it

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+    Hello World!
 </template>
 
 <script>
@@ -7,10 +8,5 @@
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
